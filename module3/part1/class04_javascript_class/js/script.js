835b778fe5831c47970aa729136e7e4ed4371085
Raphael Rabadan <raphael.rabadan@gmail.com>
2021-04-02 11:45:10 -0300

    class 04 - javascript classes

---
+++ module3/part1/class04_javascript_class/js/script.js
@@ -0,0 +1,40 @@
+class Animal {
+  constructor(name) {
+    this.name = name
+  }
+
+  speak() {
+    console.log(`${this.name} falando`)
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, type) {
+    super(name)
+    this.type = type
+  }
+
+  speak() {
+    console.log(`${this.name} (${this.type}) latindo`)
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, type) {
+    super(name)
+    this.type = type
+  }
+
+  speak() {
+    console.log(`${this.name} (${this.type}) Miando`)
+  }
+}
+
+const animal = new Animal("Totó")
+animal.speak()
+
+const dog = new Dog("Jack", "Poodle")
+dog.speak()
+
+const cat = new Cat("Moonlight", "Some cat")
+cat.speak()
