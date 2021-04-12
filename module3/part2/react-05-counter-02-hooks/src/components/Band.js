59246124b589bdfaba3b2709613d7ef54fb47546
Raphael Rabadan <raphael.rabadan@gmail.com>
2021-04-12 15:32:56 -0300

    updating this project to use hooks with function components

--- module3/part2/react-05-counter-02-hooks/src/components/Band.js
+++ module3/part2/react-05-counter-02-hooks/src/components/Band.js
@@ -1,46 +1,39 @@
-import React, { Component } from "react"
+import React, { Component, useState } from 'react'
 
-export default class Band extends Component {
-  constructor() {
-    super()
+const BAND_MEMBERS = [
+  {
+    id: 1,
+    name: 'Neil Peart',
+    instrument: 'Battery',
+  },
+  {
+    id: 2,
+    name: 'Alex Lifeson',
+    instrument: 'Guitar',
+  },
+  {
+    id: 3,
+    name: 'Neil Peart',
+    instrument: 'Bass',
+  },
+]
 
-    this.state = {
-      bandName: "Rush",
-      bandMembers: [
-        {
-          id: 1,
-          name: "Neil Peart",
-          instrument: "Battery",
-        },
-        {
-          id: 2,
-          name: "Alex Lifeson",
-          instrument: "Guitar",
-        },
-        {
-          id: 3,
-          name: "Neil Peart",
-          instrument: "Bass",
-        },
-      ],
-    }
-  }
-  render() {
-    const { bandName, bandMembers } = this.state
+export default function Band() {
+  const [bandMembers, setBandMembers] = useState(BAND_MEMBERS)
+  const [bandName, setBandName] = useState('Rush')
 
-    return (
-      <div>
-        <h5>{bandName}</h5>
-        <ul>
-          {bandMembers.map(({ id, name, instrument }) => {
-            return (
-              <li key={id}>
-                {name} - {instrument}
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h5>{bandName}</h5>
+      <ul>
+        {bandMembers.map(({ id, name, instrument }) => {
+          return (
+            <li key={id}>
+              {name} - {instrument}
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
