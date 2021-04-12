eec9ff8dc3214d5e3b89158c7aff9deee499422b
Raphael Rabadan <raphael.rabadan@gmail.com>
2021-04-12 15:39:02 -0300

    updating counter

--- module3/part2/react-05-counter-02-hooks/src/components/Counter/Counter.js
+++ module3/part2/react-05-counter-02-hooks/src/components/Counter/Counter.js
@@ -1,47 +1,26 @@
-import React, { Component } from "react"
-import css from "./counter.module.css"
-import DecrementButton from "./DecrementButton"
-import IncrementButton from "./IncrementButton"
-import Steps from "./Steps"
-import Value from "./Value"
+import React, { Component, useState } from 'react'
+import css from './counter.module.css'
+import DecrementButton from './DecrementButton'
+import IncrementButton from './IncrementButton'
+import Steps from './Steps'
+import Value from './Value'
 
-export default class Counter extends Component {
-  constructor() {
-    super()
+export default function Counter() {
+  const [currentCounter, setCurrentCounter] = useState(2)
+  const [steps, setSteps] = useState(0)
 
-    this.state = {
-      currentCounter: 2,
-      steps: 0,
-    }
+  const handleButtonClick = (clickType) => {
+    const counter = clickType === '+' ? currentCounter + 1 : currentCounter - 1
+    setCurrentCounter(counter)
+    setSteps(steps + 1)
   }
 
-  handleButtonClick = (clickType) => {
-    const { currentCounter, steps } = this.state
-    this.setState(
-      {
-        currentCounter:
-          clickType === "+" ? currentCounter + 1 : currentCounter - 1,
-        steps: steps + 1,
-      },
-      () => {
-        const { currentCounter, steps } = this.state
-        console.log(
-          `state updated with steps ${steps} and currentCounter ${currentCounter}`
-        )
-      }
-    )
-  }
-
-  render() {
-    const { currentCounter, steps } = this.state
-
-    return (
-      <div className={css.counterContainer}>
-        <DecrementButton onDecrement={this.handleButtonClick}></DecrementButton>
-        <Value value={currentCounter}></Value>
-        <IncrementButton onIncrement={this.handleButtonClick}></IncrementButton>
-        <Steps current={steps}></Steps>
-      </div>
-    )
-  }
+  return (
+    <div className={css.counterContainer}>
+      <DecrementButton onDecrement={handleButtonClick}></DecrementButton>
+      <Value value={currentCounter}></Value>
+      <IncrementButton onIncrement={handleButtonClick}></IncrementButton>
+      <Steps current={steps}></Steps>
+    </div>
+  )
 }
