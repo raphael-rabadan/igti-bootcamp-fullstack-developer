50565ccda44d5d9b35420ca066b42418ae4f42d6
Raphael Rabadan <raphael.rabadan@gmail.com>
2021-04-12 16:32:51 -0300

    challenge converted to hooks

--- module3/part2/react-07-challenge-hooks/src/App.js
+++ module3/part2/react-07-challenge-hooks/src/App.js
@@ -1,103 +1,95 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react'
 
-import CountryList from './components/Countries/CountryList';
+import CountryList from './components/Countries/CountryList'
 
-import css from './app.module.css';
-import { getTotalPopulation } from './helpers/reduce-helpers';
-import { formatNumber } from './helpers/format-helpers';
+import css from './app.module.css'
+import { getTotalPopulation } from './helpers/reduce-helpers'
+import { formatNumber } from './helpers/format-helpers'
 
-export default class App extends Component {
-  constructor() {
-    super();
+export default function App() {
+  const [allCountries, setAllCountries] = useState([])
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+  const [countryCount, setCountryCount] = useState(0)
+  const [totalPopulation, setTotalPopulation] = useState(0)
+  const [formattedPopulation, setFormattedPopulation] = useState(0)
 
-    this.state = {
-      allCountries: [],
-      currentCountries: [],
-      filter: '',
-      countryCount: 0,
-      totalPopulation: 0,
-      formattedPopulation: '0',
-    };
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const res = await fetch('https://restcountries.eu/rest/v2/all')
+      const json = await res.json()
+
+      const countries = json.map((item) => {
+        const { name, flag, numericCode, population, area } = item
+
+        return {
+          id: numericCode,
+          nameFilter: name.toLowerCase(),
+          name,
+          flag,
+          population,
+          area,
+        }
+      })
+
+      const calculatedPopulation = getTotalPopulation(countries)
+
+      setAllCountries(countries)
+      setCountries(Object.assign([], countries))
+      setCountryCount(countries.length)
+      setTotalPopulation(calculatedPopulation)
+      setFormattedPopulation(formatNumber(calculatedPopulation))
+    }
+
+    fetchCountries()
+
+    return () => {
+      console.log('ok')
+    }
+  }, [])
+
+  const handleFilter = (event) => {
+    const filterText = event.target.value
+    const filterTextLowerCase = filterText.toLowerCase()
+
+    setFilter(filterText)
+
+    const countriesFiltered = allCountries.filter((country) => {
+      return country.nameFilter.includes(filterTextLowerCase)
+    })
+
+    setCountries(countriesFiltered)
+    setCountryCount(countriesFiltered.length)
+    setTotalPopulation(getTotalPopulation(countriesFiltered))
+    setFormattedPopulation(formatNumber(totalPopulation))
   }
 
-  async componentDidMount() {
-    const res = await fetch('https://restcountries.eu/rest/v2/all');
-    const json = await res.json();
+  //const { countries, filter, countryCount, formattedPopulation } = this.state
 
-    const countries = json.map((item) => {
-      const { name, flag, numericCode, population, area } = item;
-
-      return {
-        id: numericCode,
-        nameFilter: name.toLowerCase(),
-        name,
-        flag,
-        population,
-        area,
-      };
-    });
-
-    const totalPopulation = getTotalPopulation(countries);
-
-    this.setState({
-      allCountries: countries,
-      countries,
-      countryCount: countries.length,
-      totalPopulation,
-      formattedPopulation: formatNumber(totalPopulation),
-    });
-  }
-
-  handleFilter = (event) => {
-    const filterText = event.target.value;
-    const filterTextLowerCase = filterText.toLowerCase();
-
-    this.setState({ filter: filterText });
-
-    let { countries, allCountries } = this.state;
-
-    countries = allCountries.filter((country) => {
-      return country.nameFilter.includes(filterTextLowerCase);
-    });
-
-    const totalPopulation = getTotalPopulation(countries);
-
-    this.setState({
-      countries,
-      countryCount: countries.length,
-      totalPopulation,
-      formattedPopulation: formatNumber(totalPopulation),
-    });
-  };
-
-  render() {
-    const { countries, filter, countryCount, formattedPopulation } = this.state;
-
-    return (
-      <div className={css.mainContainer}>
-        <div className={css.flexRow}>
-          <div className="input-field">
-            <input
-              placeholder="Filtro"
-              type="text"
-              value={filter}
-              onChange={this.handleFilter}
-            />
-          </div>
-
-          <div className={css.leftRightSpace}>
-            | Quantidade de países: <strong>{countryCount}</strong>
-          </div>
-
-          <div className={css.leftRightSpace}>
-            | População total: <strong>{formattedPopulation}</strong>
-          </div>
+  return (
+    <div className={css.mainContainer}>
+      <div className={css.flexRow}>
+        <div className="input-field">
+          <input
+            placeholder="Filtro"
+            type="text"
+            value={filter}
+            onChange={handleFilter}
+          />
         </div>
-        <hr />
-        <div>
-          <CountryList data={countries} />
+
+        <div className={css.leftRightSpace}>
+          | Quantidade de países: <strong>{countryCount}</strong>
+        </div>
+
+        <div className={css.leftRightSpace}>
+          | População total: <strong>{formattedPopulation}</strong>
         </div>
       </div>
-    );
-  }
+      <hr />
+      <div>
+        <CountryList data={countries} />
+      </div>
+    </div>
+  )
 }
