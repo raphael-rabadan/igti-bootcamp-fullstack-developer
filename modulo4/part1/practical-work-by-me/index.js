fa86a78410f827e29a6c5eb81c660412116cd22b
Raphael Rabadan <raphael.rabadan@gmail.com>
2021-04-21 14:40:03 -0300

    withdraw and deposit working

--- modulo4/part1/practical-work-by-me/index.js
+++ modulo4/part1/practical-work-by-me/index.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import logger from './helper/logger.js'
 
-//import { studentRouter } from './routes/studentRoutes.js'
+import accountRouter from './routes/account.js'
 
 const app = express()
 
@@ -10,20 +11,30 @@ const user = 'mongouser'
 const pass = 'mongopass1234'
 const db = 'module4-pratical-work'
 const uri = `mongodb+srv://${user}:${pass}@googlecloudbr.abil8.mongodb.net/${db}?retryWrites=true&w=majority`
-const paramsDb = { useNewUrlParser: true, useUnifiedTopology: true }
+const paramsDb = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+}
 
 try {
   await mongoose.connect(uri, paramsDb)
-  console.log('Connected to MongoDB server')
+  logger.info('Connected to MongoDB server')
 } catch (error) {
-  console.log('Error connecting to the MongoDB server')
+  logger.error('Error connecting to the MongoDB server')
 }
 
 app.use(express.json())
 app.use(cors())
-//app.use(studentRouter)
+app.use(express.static('public'))
+app.use('/account', accountRouter)
+
+app.use((err, req, res, next) => {
+  logger.error(` ${req.method} ${req.baseUrl} - ${err.message}`)
+  res.status(400).send({ error: err.message })
+})
 
 const serverPort = '3030'
 app.listen(serverPort, () =>
-  console.log(`API server running at port ${serverPort}`)
+  logger.info(`API server running at port ${serverPort}`)
 )
